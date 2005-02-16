a1ea25533ebf357d49a1b02cff2d98a6391f6c31
raasch <raasch>
2005-02-16 14:28:12 +0000

    changed Makefile for test programs to speed up current development (mature test programs are not automatically compiled when calling 'make tests'); B-spline recursion formula works

--- numerics/splines.h
+++ numerics/splines.h
@@ -18,12 +18,38 @@
 namespace MathTL
 {
   /*!
+    evaluate a B-spline N_{j,d}(x) via recursion
+  */
+  template <int d>
+  double evaluate_Bspline(const Array1D<double>& knots, const int j, const double x)
+  {
+    double r(0);
+
+    double diff = knots[j+d-1] - knots[j];
+    if (diff > 0) r += (x - knots[j]) * evaluate_Bspline<d-1>(knots, j, x) / diff;
+    diff = knots[j+d] - knots[j+1];
+    if (diff > 0) r += (knots[j+d] - x) * evaluate_Bspline<d-1>(knots, j+1, x) / diff;
+    
+    return r;
+  }
+
+  /*!
+    evaluate a B-spline N_{j,1}(x) = \chi_{[t_j,t_{j+1})}
+  */
+  template <>
+  inline
+  double evaluate_Bspline<1>(const Array1D<double>& knots, const int j, const double x)
+  {
+    return (x >=  knots[j] && x < knots[j+1] ? 1.0 : 0.0);
+  }
+
+  /*!
     This class models (compactly supported) splines of order d
-      f(x) = sum_{j=0}^n alpha_j B_{j,d}(x)
+      f(x) = sum_{j=0}^n alpha_j N_{j,d}(x)
     with respect to the nondecreasing knot sequence
-      t_0 <= t_1 <= ... <= t_n,
+      t_0 <= t_1 <= ... <= t_{n+d},
     where
-      B_{j,d}(x) = (t_{j+d}-t_j)[t_j,...,t_{j+d}](.-x)^{d-1}_+
+      N_{j,d}(x) = (t_{j+d}-t_j)[t_j,...,t_{j+d}](.-x)^{d-1}_+
     is the j-th (normalized) B-spline of order d.
 
     References:
@@ -38,10 +64,12 @@ namespace MathTL
       the default knot sequence is {0,1,...,d} to house a cardinal B-spline
     */
     Spline()
-      : Function<1>(1), knots_(d+1)
+      : Function<1>(1), knots_(d+1), coeffs_(1)
     {
       for (unsigned int i(0); i <= d; i++)
 	knots_[i] = i;
+
+      coeffs_[0] = 1.0; // for testing purposes
     }
 
     /*!
@@ -60,10 +88,10 @@ namespace MathTL
     /*!
       value of a spline
     */
-    double value(const Point<1>& p,
-		 const unsigned int component = 0) const
+    inline double value(const Point<1>& p,
+			const unsigned int component = 0) const
     {
-      return 0;
+      return evaluate_Bspline<d>(knots_, 0, p(0));
     }
   
     /*!
@@ -76,16 +104,21 @@ namespace MathTL
       values[0] = value(p);
     }
 
-    void dump()
-    {
-      std::cout << knots_ << std::endl;
-    }
+//     void dump()
+//     {
+//       std::cout << knots_ << std::endl;
+//     }
     
   protected:
     /*!
-      the knot sequence t_0,...,t_n
+      knot sequence t_0,...,t_{n+d}
      */
     Array1D<double> knots_;
+
+    /*!
+      B-spline coefficients
+    */
+    Array1D<double> coeffs_;
   };
 }
 
--- tests/test_splines.cpp
+++ tests/test_splines.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <geometry/point.h>
 #include <numerics/splines.h>
 
 using std::cout;
@@ -9,6 +10,21 @@ int main()
 {
   cout << "Testing MathTL::Spline ..." << endl;
 
-  Spline<2> f;
-  f.dump();
+  cout << "* some point values of the default constant spline:" << endl;
+  for (double x = -0.5; x <= 1.5; x+=0.5) {
+    cout << "  N(" << x << ")="
+	 <<  Spline<1>().value(Point<1>(x)) << endl;
+  }
+
+  cout << "* some point values of the default linear spline:" << endl;
+  for (double x = -0.5; x <= 2.5; x+=0.5) {
+    cout << "  N(" << x << ")="
+	 <<  Spline<2>().value(Point<1>(x)) << endl;
+  }
+
+  cout << "* some point values of the default quadratic spline:" << endl;
+  for (double x = -0.5; x <= 3.5; x+=0.5) {
+    cout << "  N(" << x << ")="
+	 <<  Spline<3>().value(Point<1>(x)) << endl;
+  }
 }

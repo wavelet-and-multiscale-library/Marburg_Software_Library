f170bf8c64f877aa1920cf0712c0b5af6971bc7a
raasch <raasch>
2005-02-17 14:53:36 +0000

    updated comprehensive include file

--- MathTL.h
+++ MathTL.h
@@ -15,35 +15,39 @@
 #include <utils/array1d.h>
 #include <utils/function.h>
 #include <utils/function_time.h>
-#include <utils/tiny_tools.h>
 #include <utils/random.h>
+#include <utils/tiny_tools.h>
 
-#include <algebra/tensor.h>
-#include <algebra/tensor_base.h>
-#include <algebra/vector.h>
+#include <algebra/atra.h>
 #include <algebra/infinite_vector.h>
-#include <algebra/vector_norms.h>
-#include <algebra/vector_arithmetics.h>
+#include <algebra/laurent_polynomial.h>
 #include <algebra/matrix.h>
 #include <algebra/matrix_norms.h>
-#include <algebra/triangular_matrix.h>
-#include <algebra/symmetric_matrix.h>
-#include <algebra/atra.h>
 #include <algebra/polynomial.h>
-#include <algebra/laurent_polynomial.h>
+#include <algebra/symmetric_matrix.h>
+#include <algebra/tensor_base.h>
+#include <algebra/tensor.h>
+#include <algebra/triangular_matrix.h>
+#include <algebra/vector_arithmetics.h>
+#include <algebra/vector.h>
+#include <algebra/vector_norms.h>
 
-#include <io/vector_io.h>
 #include <io/matrix_io.h>
+#include <io/vector_io.h>
 
-#include <geometry/point.h>
 #include <geometry/grid.h>
+#include <geometry/point.h>
+#include <geometry/sampled_mapping.h>
 
-#include <numerics/iteratsolv.h>
+#include <numerics/b_splines.h>
 #include <numerics/eigenvalues.h>
+#include <numerics/gauss_data.h>
+#include <numerics/gauss_quadrature.h>
+#include <numerics/iteratsolv.h>
 #include <numerics/matrix_decomp.h>
 #include <numerics/ortho_poly.h>
 #include <numerics/quadrature.h>
-#include <numerics/gauss_quadrature.h>
 #include <numerics/recursion.h>
+#include <numerics/splines.h>
 
 #endif

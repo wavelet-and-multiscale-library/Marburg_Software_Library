97a83116eccba09615d111fbf6e243f045effc7f
raasch <raasch>
2005-03-02 08:32:34 +0000

    forgot to add a file

---
+++ utils/multiindex.cpp
@@ -0,0 +1,26 @@
+// implementation for multiindex.h
+
+namespace MathTL
+{
+  template <class I, unsigned int DIMENSION>
+  MultiIndex<I, DIMENSION>::MultiIndex()
+    : Array1D<I>(DIMENSION)
+  {
+  }
+
+  template <class I, unsigned int DIMENSION>
+  bool MultiIndex<I, DIMENSION>::operator == (const MultiIndex& lambda) const
+  {
+    for (unsigned int i(0); i < DIMENSION; i++)
+      if (Array1D<I>::operator [] (i) != lambda[i]) return false;
+    return true;
+  }
+
+  template <class I, unsigned int DIMENSION>
+  bool MultiIndex<I, DIMENSION>::operator < (const MultiIndex& lambda) const
+  {
+    for (unsigned int i(0); i < DIMENSION; i++)
+      if (Array1D<I>::operator [] (i) < lambda[i]) return true;
+    return false;
+  }
+}

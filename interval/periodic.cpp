8fb9de334c8059d0565b9e9550d06b72429ab04e
raasch <raasch>
2005-09-16 15:13:45 +0000

    decompose_t(), reconstruct_t() for PeriodicBasis

--- interval/periodic.cpp
+++ interval/periodic.cpp
@@ -117,46 +117,48 @@ namespace WaveletTL
   PeriodicBasis<RBASIS>::decompose_t_1(const Index& lambda,
 				       const int j0,
 				       InfiniteVector<double, Index>& c) const {
-//     assert(lambda.j() >= j0);
-//     c.clear();
-//     if (lambda.e() == 1)
-//       {
-// 	// the true wavelet coefficients don't have to be modified
-// 	c[lambda] = 1.0;
-//       }
-//     else
-//       {
-// 	// a generator on a (possibly) fine level
-// 	if (lambda.j() == j0)
-// 	  {
-// 	    // generators from the coarsest level can be copied
-// 	    c[lambda] = 1.0;
-// 	  }
-// 	else
-// 	  {
-// 	    // j>j0, perform multiscale decomposition
-	    
-// 	    const int abegin = a().begin().index()[0];
-// 	    const int aend   = a().rbegin().index()[0];
-// 	    const int bbegin = b().begin().index()[0];
-// 	    const int bend   = b().rbegin().index()[0];
-	    
-// 	    // compute d_{j-1}
-//  	    for (int l((int)ceil((lambda.k()-bend)/2.0));
-// 		 l <= (int)floor((lambda.k()-bbegin)/2.0); l++)
-//  	      c[Index(lambda.j()-1, 1, l)] =
-// 		M_SQRT1_2 * b().get_coefficient(MultiIndex<int, 1>(lambda.k()-2*l));
-	    
-// 	    // compute c_{j_0} via recursion
-// 	    for (int l((int)ceil((lambda.k()-aend)/2.0));
-// 		 l <= (int)floor((lambda.k()-abegin)/2.0); l++)
-// 	      {
-// 		InfiniteVector<double, Index> d;
-// 		decompose_t_1(Index(lambda.j()-1, 0, l), j0, d);
-// 		c += M_SQRT1_2 * a().get_coefficient(MultiIndex<int, 1>(lambda.k()-2*l)) * d;
-// 	      }
-// 	  }
-//       }
+    assert(lambda.j() >= j0);
+    c.clear();
+    if (lambda.e() == 1) {
+      // the true wavelet coefficients don't have to be modified
+      c.set_coefficient(lambda, 1.0);
+    } else {
+      // a generator on a (possibly) fine level
+      if (lambda.j() == j0) {
+	// generators from the coarsest level can be copied
+	c.set_coefficient(lambda, 1.0);
+      }	else {
+	// j>j0, perform multiscale decomposition
+	
+	const int abegin = rbasis.a().begin().index()[0];
+	const int aend   = rbasis.a().rbegin().index()[0];
+	const int bbegin = rbasis.b().begin().index()[0];
+	const int bend   = rbasis.b().rbegin().index()[0];
+	
+	// compute d_{j-1}
+	for (int n = 0; n < 1<<(lambda.j()-1); n++) {
+	  double cn = 0;
+	  for (int m = (int) ceil(ldexp(1.0, 1-lambda.j()) * ((lambda.k() - bend) / 2.0 - n));
+	       m <= (int) floor(ldexp(1.0, 1-lambda.j()) * ((lambda.k() - bbegin) / 2.0 - n));
+	       m++)
+	    cn += rbasis.b().get_coefficient(MultiIndex<int, 1>(lambda.k()-2*((1<<(lambda.j()-1))*m+n)));
+	  if (cn != 0)
+	    c[Index(lambda.j()-1, 1, n, this)] = M_SQRT1_2 * cn;
+	}
+	
+	// compute c_{j_0} via recursion
+	for (int n = 0; n < 1<<(lambda.j()-1); n++) {
+	  double cn = 0;
+	  for (int m = (int) ceil(ldexp(1.0, 1-lambda.j()) * ((lambda.k() - aend) / 2.0 - n));
+	       m <= (int) floor(ldexp(1.0, 1-lambda.j()) * ((lambda.k() - abegin) / 2.0 - n));
+	       m++)
+	    cn += rbasis.a().get_coefficient(MultiIndex<int, 1>(lambda.k()-2*((1<<(lambda.j()-1))*m+n)));
+	  InfiniteVector<double,Index> d;
+	  decompose_t_1(Index(lambda.j()-1, 0, n, this), j0, d);
+	  c += M_SQRT1_2 * cn * d;
+	}
+      }
+    }
   }
 
   template <class RBASIS>
@@ -209,41 +211,46 @@ namespace WaveletTL
   void
   PeriodicBasis<RBASIS>::reconstruct_t_1(const Index& lambda,
 					 const int j,
-					 InfiniteVector<double, Index>& c) const
-  {
-//     if (lambda.j() >= j)
-//       {
-// 	// then we can just copy \psi_\lambda
-// 	c[lambda] += 1.0;
-//       }
-//     else
-//       {
-// 	// reconstruct by recursion
-
-// 	const int aTbegin = aT().begin().index()[0];
-// 	const int aTend   = aT().rbegin().index()[0];
-// 	const int bTbegin = bT().begin().index()[0];
-// 	const int bTend   = bT().rbegin().index()[0];
-	    
-// 	if (lambda.e() == 0)
-// 	  {
-//  	    for (int l(2*lambda.k()+aTbegin); l <= 2*lambda.k()+aTend; l++)
-//  	      {
-//  		InfiniteVector<double, Index> d;
-//  		reconstruct_t_1(Index(lambda.j()+1, 0, l), j, d);
-//  		c += M_SQRT1_2 * aT().get_coefficient(MultiIndex<int, 1>(l-2*lambda.k())) * d;
-//  	      }
-//  	  }
-//  	else
-//  	  {
-//  	    for (int l(2*lambda.k()+bTbegin); l <= 2*lambda.k()+bTend; l++)
-//  	      {
-//  		InfiniteVector<double, Index> d;
-//  		reconstruct_t_1(Index(lambda.j()+1, 0, l), j, d);
-//  		c += M_SQRT1_2 * bT().get_coefficient(MultiIndex<int, 1>(l-2*lambda.k())) * d;
-//  	      }
-// 	  }
-//       }
+					 InfiniteVector<double, Index>& c) const {
+    if (lambda.j() >= j) {
+      // then we can just copy \psi_\lambda
+      c.set_coefficient(lambda, 1.0);
+    } else {
+      // reconstruct by recursion
+      
+      const int aTbegin = rbasis.aT().begin().index()[0];
+      const int aTend   = rbasis.aT().rbegin().index()[0];
+      const int bTbegin = rbasis.bT().begin().index()[0];
+      const int bTend   = rbasis.bT().rbegin().index()[0];
+      
+      if (lambda.e() == 0) {
+	for (int n = 0; n < 1<<(lambda.j()+1); n++) {
+	  double cn = 0;
+	  for (int m = (int) ceil(ldexp(1.0, -lambda.j()-1) * (aTbegin+2*lambda.k()-n));
+	       m <= (int) floor(ldexp(1.0, -lambda.j()-1) * (aTend+2*lambda.k()-n));
+	       m++)
+	    cn += rbasis.aT().get_coefficient(MultiIndex<int, 1>((1<<(lambda.j()+1))*m+n-2*lambda.k()));
+	  if (cn != 0) {
+	    InfiniteVector<double,Index> d;
+	    reconstruct_t_1(Index(lambda.j()+1, 0, n, this), j, d);
+	    c += M_SQRT1_2 * cn * d;
+	  }
+	}
+      } else {
+	for (int n = 0; n < 1<<(lambda.j()+1); n++) {
+	  double cn = 0;
+	  for (int m = (int) ceil(ldexp(1.0, -lambda.j()-1) * (bTbegin+2*lambda.k()-n));
+	       m <= (int) floor(ldexp(1.0, -lambda.j()-1) * (bTend+2*lambda.k()-n));
+	       m++)
+	    cn += rbasis.bT().get_coefficient(MultiIndex<int, 1>((1<<(lambda.j()+1))*m+n-2*lambda.k()));
+	  if (cn != 0) {
+	    InfiniteVector<double,Index> d;
+	    reconstruct_t_1(Index(lambda.j()+1, 0, n, this), j, d);
+	    c += M_SQRT1_2 * cn * d;
+	  }
+	}
+      }
+    }
   }
 
 }
--- tests/test_periodic.cpp
+++ tests/test_periodic.cpp
@@ -47,6 +47,32 @@ int main()
     }
 #endif
 
+#if 1
+  for (int level = basis.j0()+1; level <= basis.j0()+2; level++)
+    {
+      cout << "- checking decompose_t() and reconstruct_t() for some/all generators on the level "
+	   << level << ":" << endl;
+      Index index(first_generator(&basis, level));
+      for (;; ++index)
+	{
+	  InfiniteVector<double, Index> origcoeff;
+	  origcoeff[index] = 1.0;
+	  
+	  InfiniteVector<double, Index> wcoeff;
+	  basis.decompose_t(origcoeff, basis.j0(), wcoeff);
+	  
+	  InfiniteVector<double, Index> transformcoeff;
+	  basis.reconstruct_t(wcoeff, level, transformcoeff);
+	  
+	  cout << "* generator: " << index
+	       << ", max. error: " << linfty_norm(origcoeff-transformcoeff) << endl;
+	  
+	  if (index == last_generator(&basis, level)) break;
+	}
+    }
+#endif
+
+
   // ---------------------------------------------------------------------------
 
   cout << "* another periodic basis:" << endl;
@@ -82,6 +108,33 @@ int main()
 	  cout << "* generator: " << index
 	       << ", max. error: " << linfty_norm(origcoeff-transformcoeff) << endl;
 	  
+	  if (index == last_generator(&basis2, level)) break;
+	}
+    }
+#endif
+
+#if 1
+  for (int level = basis2.j0()+1; level <= basis2.j0()+2; level++)
+    {
+      cout << "- checking decompose() and reconstruct() for some/all generators on the level "
+	   << level << ":" << endl;
+      Index2 index(first_generator(&basis2, level));
+      for (;; ++index)
+	{
+	  InfiniteVector<double, Index2> origcoeff;
+	  origcoeff[index] = 1.0;
+	  
+	  InfiniteVector<double, Index2> wcoeff;
+ 	  basis2.decompose(origcoeff, basis2.j0(), wcoeff);
+	  
+	  InfiniteVector<double, Index2> transformcoeff;
+	  basis2.reconstruct(wcoeff, level, transformcoeff);
+
+	  double error = linfty_norm(origcoeff-transformcoeff);
+	  
+	  cout << "* generator: " << index
+	       << ", max. error: " << error << endl;
+
 	  if (index == last_generator(&basis2, level)) break;
 	}
     }

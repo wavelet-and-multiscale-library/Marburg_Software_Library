c8d81b0a0ea4471d2e9432314ed8f03eb8eaa8a2
raasch <raasch>
2005-10-21 16:47:22 +0000

    minor modification, test program still a dummy one

--- tests/test_cube_equation.cpp
+++ tests/test_cube_equation.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 
+#include <algebra/vector.h>
+#include <utils/function.h>
+#include <numerics/bvp.h>
 #include <galerkin/cube_equation.h>
 
+using namespace MathTL;
 using namespace WaveletTL;
 
 int main()
 {
   cout << "Testing wavelet-Galerkin solution of an elliptic equation on the cube ..." << endl;
+
+  ConstantFunction<2> rhs(Vector<double>(1, "1.0"));
+  PoissonBVP<2> poisson(&rhs);
+
 
 
   return 0;

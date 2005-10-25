77cb61c5cfa5ab8ee5dff5e2fe91b0665abae761
raasch <raasch>
2005-10-25 08:00:06 +0000

    split implementation of SampledMapping into two files

---
+++ geometry/sampled_mapping.cpp
@@ -0,0 +1,105 @@
+// implementation for sampled_mapping.h
+
+namespace MathTL
+{
+  SampledMapping<1>::SampledMapping()
+    : Grid<1>(), values_()
+  {
+  }
+
+  SampledMapping<1>::SampledMapping(const SampledMapping<1>& sm)
+    : Grid<1>(sm), values_(sm.values_)
+  {
+  }
+
+  SampledMapping<1>::SampledMapping(const Grid<1>& grid, const Array1D<double>& values)
+    : Grid<1>(grid), values_(values)
+  {
+  }
+
+  SampledMapping<1>::SampledMapping(const Grid<1>& grid, const Function<1>& f)
+    : Grid<1>(grid), values_(grid.size())
+  {
+    for (unsigned int n(0); n < grid.size(); n++)
+      values_[n] = f.value(Point<1>(grid_[n]));
+  }
+  
+  SampledMapping<1>::SampledMapping(const MultiIndex<int, 1>& a,
+				    const MultiIndex<int, 1>& b,
+				    const InfiniteVector<double, MultiIndex<int, 1> >& values,
+				    const int resolution)
+    : Grid<1>(a[0], b[0], (1<<resolution)*(b[0]-a[0]))
+  {
+    values_.resize(Grid<1>::size());
+    for (int k(a[0]<<resolution), n(0); k <= (b[0]<<resolution); k++, n++)
+      values_[n] = values.get_coefficient(MultiIndex<int, 1>(k));
+  }
+  
+  SampledMapping<1>& 
+  SampledMapping<1>::operator = (const SampledMapping<1>& sm)
+  {
+    Grid<1>::operator = (sm);
+    values_ = sm.values_;
+    return *this;
+  }
+
+  void
+  SampledMapping<1>::matlab_output(std::ostream& os) const
+  {
+    Grid<1>::matlab_output(os);
+    os << "y = " // here we can take y
+       << values_
+       << ";"
+       << std::endl;
+  }
+
+  SampledMapping<2>::SampledMapping()
+    : Grid<2>(), values_()
+  {
+  }
+
+  SampledMapping<2>::SampledMapping(const SampledMapping<2>& sm)
+    : Grid<2>(sm), values_(sm.values_)
+  {
+  }
+
+  SampledMapping<2>::SampledMapping(const Grid<2>& grid, const Matrix<double>& values)
+    : Grid<2>(grid), values_(values)
+  {
+  }
+
+  SampledMapping<2>::SampledMapping(const Grid<2>& grid, const Function<2>& f)
+    : Grid<2>(grid), values_()
+  {
+    values_.resize(gridx_.row_dimension(), gridx_.column_dimension());
+    for (unsigned int m(0); m < values_.row_dimension(); m++)
+      for (unsigned int n(0); n < values_.column_dimension(); n++)
+	values_(m,n) = f.value(Point<2>(gridx_(m,n), gridy_(m,n)));
+  }
+  
+  SampledMapping<2>::SampledMapping(const MultiIndex<int, 2>& a,
+				    const MultiIndex<int, 2>& b,
+				    const InfiniteVector<double, MultiIndex<int, 2> >& values,
+				    const int resolution)
+  {
+  }
+
+  SampledMapping<2>&
+  SampledMapping<2>::operator = (const SampledMapping<2>& sm)
+  {
+    Grid<2>::operator = (sm);
+    values_ = sm.values_;
+    return *this;
+  }
+
+  void
+  SampledMapping<2>::matlab_output(std::ostream& os) const
+  {
+    Grid<2>::matlab_output(os);
+    os << "z = ";
+    print_matrix(values_, os);
+    os << ";"
+       << std::endl;
+  }
+  
+}
--- geometry/sampled_mapping.h
+++ geometry/sampled_mapping.h
@@ -83,33 +83,22 @@ namespace MathTL
     /*!
       default constructor, yields empty mapping
     */
-    SampledMapping() : Grid<1>(), values_() {}
+    SampledMapping();
 
     /*!
       copy constructor
     */
-    SampledMapping(const SampledMapping<1>& sm)
-      : Grid<1>(sm), values_(sm.values_)
-    {
-    }
+    SampledMapping(const SampledMapping<1>& sm);
 
     /*!
       constructor from a given grid and given values
     */
-    SampledMapping(const Grid<1>& grid, const Array1D<double>& values)
-      : Grid<1>(grid), values_(values)
-    {
-    }
+    SampledMapping(const Grid<1>& grid, const Array1D<double>& values);
 
     /*!
       constructor from a fixed grid and a Function object
     */
-    SampledMapping(const Grid<1>& grid, const Function<1>& f)
-      : Grid<1>(grid), values_(grid.size())
-    {
-      for (unsigned int n(0); n < grid.size(); n++)
-	values_[n] = f.value(Point<1>(grid_[n]));
-    }
+    SampledMapping(const Grid<1>& grid, const Function<1>& f);
 
     /*!
       constructor from given values on 2^{-resolution}\mathbb Z, clipped to [a,b]
@@ -117,23 +106,12 @@ namespace MathTL
     SampledMapping(const MultiIndex<int, 1>& a,
 		   const MultiIndex<int, 1>& b,
 		   const InfiniteVector<double, MultiIndex<int, 1> >& values,
-		   const int resolution)
-      : Grid<1>(a[0], b[0], (1<<resolution)*(b[0]-a[0]))
-    {
-      values_.resize(Grid<1>::size());
-      for (int k(a[0]<<resolution), n(0); k <= (b[0]<<resolution); k++, n++)
-	values_[n] = values.get_coefficient(MultiIndex<int, 1>(k));
-    }
+		   const int resolution);
 
     /*!
       assignment operator
     */
-    SampledMapping<1>& operator = (const SampledMapping<1>& sm)
-    {
-      Grid<1>::operator = (sm);
-      values_ = sm.values_;
-      return *this;
-    }
+    SampledMapping<1>& operator = (const SampledMapping<1>& sm);
 
     /*!
       reading access to the function values
@@ -143,14 +121,7 @@ namespace MathTL
     /*!
       Matlab output of the sampled mapping onto a stream
     */
-    void matlab_output(std::ostream& os) const
-    {
-      Grid<1>::matlab_output(os);
-      os << "y = " // here we can take y
-	 << values_
-	 << ";"
-	 << std::endl;
-    }
+    void matlab_output(std::ostream& os) const;
 
   protected:
     /*!
@@ -167,68 +138,42 @@ namespace MathTL
     /*!
       default constructor, yields empty mapping
     */
-    SampledMapping() : Grid<2>(), values_() {}
+    SampledMapping();
 
     /*!
       copy constructor
     */
-    SampledMapping(const SampledMapping<2>& sm)
-      : Grid<2>(sm), values_(sm.values_)
-    {
-    }
+    SampledMapping(const SampledMapping<2>& sm);
 
     /*!
       constructor from a given grid and given values
     */
-    SampledMapping(const Grid<2>& grid, const Matrix<double>& values)
-      : Grid<2>(grid), values_(values)
-    {
-    }
+    SampledMapping(const Grid<2>& grid, const Matrix<double>& values);
 
     /*!
       constructor from a fixed grid and a Function object
     */
-    SampledMapping(const Grid<2>& grid, const Function<2>& f)
-      : Grid<2>(grid), values_()
-    {
-      values_.resize(gridx_.row_dimension(), gridx_.column_dimension());
-      for (unsigned int m(0); m < values_.row_dimension(); m++)
-	for (unsigned int n(0); n < values_.column_dimension(); n++)
-	  values_(m,n) = f.value(Point<2>(gridx_(m,n), gridy_(m,n)));
-    }
+    SampledMapping(const Grid<2>& grid, const Function<2>& f);
 
     /*!
       constructor from given values on 2^{-resolution}\mathbb Z^2,
       clipped to [a,b]^2
+      (CURRENTLY NOT IMPLEMENTED!)
     */
     SampledMapping(const MultiIndex<int, 2>& a,
 		   const MultiIndex<int, 2>& b,
 		   const InfiniteVector<double, MultiIndex<int, 2> >& values,
-		   const int resolution)
-    {
-    }
+		   const int resolution);
 
     /*!
       assignment operator
     */
-    SampledMapping<2>& operator = (const SampledMapping<2>& sm)
-    {
-      Grid<2>::operator = (sm);
-      values_ = sm.values_;
-      return *this;
-    }
+    SampledMapping<2>& operator = (const SampledMapping<2>& sm);
 
     /*!
       Matlab output of the sampled mapping onto a stream
     */
-    void matlab_output(std::ostream& os) const
-    {
-      Grid<2>::matlab_output(os);
-      os << "z = ";
-      print_matrix(values_, os);
-      os << ";"
-	 << std::endl;
-    }
+    void matlab_output(std::ostream& os) const;
 
   protected:
     /*!
@@ -237,4 +182,7 @@ namespace MathTL
     Matrix<double> values_;
   };
 }
+
+#include "geometry/sampled_mapping.cpp"
+
 #endif

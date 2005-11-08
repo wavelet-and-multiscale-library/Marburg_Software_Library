17b7fec59666746324ce8ada48b7819f6f680b04
werner <werner>
2005-11-08 14:55:46 +0000

    forgot to add a few files...

---
+++ steepest_descent.cpp
@@ -0,0 +1,18 @@
+// implementation for steepest_descent.h
+
+#include <cmath>
+#include <set>
+
+#include <adaptive/apply.h>
+
+using std::set;
+
+namespace FrameTL
+{
+  template <class PROBLEM>
+  void steepest_descent_SOLVE(const PROBLEM& P,  const double epsilon,
+			      InfiniteVector<double, typename PROBLEM::Index>& u_epsilon)
+  {
+    
+  }
+}
---
+++ tests/test_steepest_descent.cpp
@@ -0,0 +1,170 @@
+#include <fstream>
+#include <iostream>
+#include <time.h> 
+#include <interval/ds_basis.h>
+#include "elliptic_equation.h"
+#include <algebra/sparse_matrix.h>
+#include <algebra/infinite_vector.h>
+#include <numerics/iteratsolv.h>
+#include <numerics/eigenvalues.h>
+#include <frame_evaluate.h>
+#include <cube/cube_basis.h>
+#include <cube/cube_index.h>
+#include <galerkin/galerkin_utils.h>
+#include <frame_support.h>
+#include <frame_index.h>
+#include <steepest_descent.h>
+
+using std::cout;
+using std::endl;
+
+using FrameTL::FrameIndex;
+using FrameTL::EllipticEquation;
+using FrameTL::EvaluateFrame;
+using FrameTL::AggregatedFrame;
+using MathTL::EllipticBVP;
+using MathTL::PoissonBVP;
+using MathTL::ConstantFunction;
+using MathTL::SparseMatrix;
+using MathTL::InfiniteVector;
+using WaveletTL::CubeBasis;
+using WaveletTL::CubeIndex;
+
+using namespace std;
+using namespace FrameTL;
+using namespace MathTL;
+using namespace WaveletTL;
+
+
+  /*!
+    special function with steep gradients
+    near the right end of the interval
+  */
+template<class VALUE = double>
+class Singularity1D
+  : public Function<1, VALUE>
+{
+public:
+  Singularity1D() {};
+  virtual ~Singularity1D() {};
+  VALUE value(const Point<1>& p,
+	      const unsigned int component = 0) const
+  {
+    return  -100*exp(5*p[0])*(1-(exp(5*p[0])-1)/(exp(5.)-1))/(exp(5.)-1)+200*exp(10*p[0]) / 
+      ((exp(5.)-1)*(exp(5.)-1))+100*(exp(5*p[0])-1)*exp(5*p[0])/((exp(5.)-1)*(exp(5.)-1));
+  }
+  
+  void vector_value(const Point<1> &p,
+		    Vector<VALUE>& values) const { ; }
+  
+};
+
+
+int main()
+{
+  
+  cout << "Testing class EllipticEquation..." << endl;
+  
+  const int DIM = 1;
+
+  typedef DSBasis<2,2> Basis1D;
+  typedef AggregatedFrame<Basis1D,1,1> Frame1D;
+  typedef CubeBasis<Basis1D,1> IntervalBasis;
+  typedef Frame1D::Index Index;
+
+  //##############################  
+  Matrix<double> A(DIM,DIM);
+  A(0,0) = 0.75;
+  Point<1> b;
+  b[0] = 0.;
+  AffineLinearMapping<1> affineP(A,b);
+  
+  Matrix<double> A2(DIM,DIM);
+  A2(0,0) = 0.75;
+  Point<1> b2;
+  b2[0] = 0.25;
+  AffineLinearMapping<1> affineP2(A2,b2);
+  //##############################
+  
+  Array1D<Chart<DIM,DIM>* > charts(2);
+  charts[0] = &affineP;
+  charts[1] = &affineP2;
+  
+  SymmetricMatrix<bool> adj(2);
+  adj(0,0) = 1;
+  adj(1,1) = 1;
+  adj(1,0) = 1;
+  adj(0,1) = 1;
+  
+  //to specify primal boundary the conditions
+  Array1D<FixedArray1D<int,2*DIM> > bc(2);
+
+  //primal boundary conditions for first patch: all Dirichlet
+  FixedArray1D<int,2*DIM> bound_1;
+  bound_1[0] = 1;
+  bound_1[1] = 1;
+
+  bc[0] = bound_1;
+
+  //primal boundary conditions for second patch: all Dirichlet
+  FixedArray1D<int,2*DIM> bound_2;
+  bound_2[0] = 1;
+  bound_2[1] = 1;
+
+  bc[1] = bound_2;
+
+//to specify primal boundary the conditions
+  Array1D<FixedArray1D<int,2*DIM> > bcT(2);
+
+  //dual boundary conditions for first patch
+  FixedArray1D<int,2*DIM> bound_3;
+  bound_3[0] = 0;
+  bound_3[1] = 0;
+
+  bcT[0] = bound_3;
+
+  //dual boundary conditions for second patch
+  FixedArray1D<int,2*DIM> bound_4;
+  bound_4[0] = 0;
+  bound_4[1] = 0;
+ 
+  bcT[1] = bound_4;
+
+  Atlas<DIM,DIM> Lshaped(charts,adj);  
+  cout << Lshaped << endl;
+
+  //finally a frame can be constructed
+  Frame1D frame(&Lshaped, bc, bcT);
+
+  Vector<double> value(1);
+  value[0] = 1;
+  ConstantFunction<DIM> const_fun(value);
+
+  Singularity1D<double> sing1D;
+  
+  //PoissonBVP<DIM> poisson(&const_fun);
+  PoissonBVP<DIM> poisson(&sing1D);
+
+  //EllipticEquation<Basis1D,DIM> discrete_poisson(&poisson, &frame, TrivialAffine);
+  EllipticEquation<Basis1D,DIM> discrete_poisson(&poisson, &frame, Composite);
+
+  const double epsilon = 0.01;
+
+  InfiniteVector<double, Index> u_epsilon;
+
+  steepest_descent_SOLVE(discrete_poisson, epsilon, u_epsilon);
+
+  discrete_poisson.rescale(u_epsilon,-1);
+
+  EvaluateFrame<Basis1D,1,1> evalObj;
+
+  Array1D<SampledMapping<1> > U = evalObj.evaluate(frame, u_epsilon, true, 10);//expand in primal basis
+
+  std::ofstream ofs5("approx_sol_steep_1D_out.m");
+  matlab_output(ofs5,U);
+  ofs5.close();
+
+
+  return 0;
+
+}

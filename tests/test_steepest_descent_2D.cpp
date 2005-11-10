b5f15fd303ce3b188ec361f36c9a876551bd3954
werner <werner>
2005-11-10 15:45:39 +0000

    once again a forgotten file

---
+++ tests/test_steepest_descent_2D.cpp
@@ -0,0 +1,206 @@
+#include <fstream>
+#include <iostream>
+#include <time.h> 
+#include <interval/ds_basis.h>
+#include "elliptic_equation.h"
+#include <algebra/sparse_matrix.h>
+#include <algebra/infinite_vector.h>
+#include <numerics/iteratsolv.h>
+#include <numerics/eigenvalues.h>
+#include <frame_evaluate.h>
+#include <cube/cube_basis.h>
+#include <cube/cube_index.h>
+#include <galerkin/galerkin_utils.h>
+#include <numerics/corner_singularity.h>
+#include <frame_support.h>
+#include <frame_index.h>
+#include <steepest_descent.h>
+#include <galerkin/cached_problem.h>
+
+using std::cout;
+using std::endl;
+
+using FrameTL::FrameIndex;
+using FrameTL::EllipticEquation;
+using FrameTL::EvaluateFrame;
+using FrameTL::AggregatedFrame;
+using MathTL::EllipticBVP;
+using MathTL::PoissonBVP;
+using MathTL::ConstantFunction;
+using MathTL::CornerSingularityRHS;
+using MathTL::CornerSingularity;
+using MathTL::SparseMatrix;
+using MathTL::InfiniteVector;
+using WaveletTL::CubeBasis;
+using WaveletTL::CubeIndex;
+using WaveletTL::CachedProblem;
+
+using namespace std;
+using namespace FrameTL;
+using namespace MathTL;
+using namespace WaveletTL;
+
+
+int main()
+{
+  
+  cout << "testing steepest descent algorithm in 2D..." << endl;
+
+  const int DIM = 2;
+
+  typedef DSBasis<2,2> Basis1D;
+  typedef AggregatedFrame<Basis1D,2,2> Frame2D;
+  typedef CubeBasis<Basis1D> Basis;
+  typedef Frame2D::Index Index;
+
+  EvaluateFrame<Basis1D,2,2> evalObj;
+
+  //##############################  
+  Matrix<double> A(DIM,DIM);
+  A(0,0) = 2.0;
+  A(1,1) = 1.0;
+  Point<2> b;
+  b[0] = -1.;
+  b[1] = -1.;
+  AffineLinearMapping<2> affineP(A,b);
+
+  Matrix<double> A2(DIM,DIM);
+  A2(0,0) = 1.0;
+  A2(1,1) = 2.0;
+  Point<2> b2;
+  b2[0] = -1.;
+  b2[1] = -1.;
+  AffineLinearMapping<2> affineP2(A2,b2);
+  //##############################
+
+  //##############################
+  LinearBezierMapping bezierP(Point<2>(-1.,-1.),Point<2>(-1.,1.),
+ 			      Point<2>(0.,-1.), Point<2>(0.,1.));
+  
+  LinearBezierMapping bezierP2(Point<2>(-1.,-1.),Point<2>(-1.,0.),
+			       Point<2>(1.,-1.), Point<2>(1.,0.));
+ 
+  //##############################
+  Array1D<Chart<DIM,DIM>* > charts(2);
+  //charts[0] = &bezierP;
+  charts[0] = &affineP;
+  //charts[1] = &bezierP2;
+  charts[1] = &affineP2;
+ 
+  SymmetricMatrix<bool> adj(2);
+  adj(0,0) = 1;
+  adj(1,1) = 1;
+  adj(1,0) = 1;
+  adj(0,1) = 1;
+  
+  //to specify primal boundary the conditions
+  Array1D<FixedArray1D<int,2*DIM> > bc(2);
+
+  //primal boundary conditions for first patch: all Dirichlet
+  FixedArray1D<int,2*DIM> bound_1;
+  bound_1[0] = 1;
+  bound_1[1] = 1;
+  bound_1[2] = 1;
+  bound_1[3] = 1;
+
+  bc[0] = bound_1;
+
+  //primal boundary conditions for second patch: all Dirichlet
+  FixedArray1D<int,2*DIM> bound_2;
+  bound_2[0] = 1;
+  bound_2[1] = 1;
+  bound_2[2] = 1;
+  bound_2[3] = 1;
+
+  bc[1] = bound_2;
+
+//to specify primal boundary the conditions
+  Array1D<FixedArray1D<int,2*DIM> > bcT(2);
+
+  //dual boundary conditions for first patch
+  FixedArray1D<int,2*DIM> bound_3;
+  bound_3[0] = 0;
+  bound_3[1] = 0;
+  bound_3[2] = 0;
+  bound_3[3] = 0;
+
+  bcT[0] = bound_3;
+
+  //dual boundary conditions for second patch
+  FixedArray1D<int,2*DIM> bound_4;
+  bound_4[0] = 0;
+  bound_4[1] = 0;
+  bound_4[2] = 0;
+  bound_4[3] = 0;
+ 
+  bcT[1] = bound_4;
+
+  Atlas<DIM,DIM> Lshaped(charts,adj);  
+  cout << Lshaped << endl;
+
+  //finally a frame can be constructed
+  AggregatedFrame<Basis1D, DIM, DIM> frame(&Lshaped, bc, bcT);
+
+  Vector<double> value(1);
+  value[0] = 1;
+  
+  ConstantFunction<DIM> const_fun(value);
+  Point<2> origin;
+  origin[0] = 0.0;
+  origin[1] = 0.0;
+
+  CornerSingularity sing2D(origin, 0.5, 1.5);
+  CornerSingularityRHS singRhs(origin, 0.5, 1.5);
+  
+  PoissonBVP<DIM> poisson(&singRhs);
+  //PoissonBVP<DIM> poisson(&const_fun);
+
+  EllipticEquation<Basis1D,DIM> discrete_poisson(&poisson, &frame, TrivialAffine);
+  //EllipticEquation<Basis1D,DIM> discrete_poisson(&poisson, &frame, Composite);
+
+  discrete_poisson.set_norm_A(10.);
+  discrete_poisson.set_Ainv(10.);
+
+  CachedProblem<EllipticEquation<Basis1D,DIM> > problem(&discrete_poisson, 10.0, 0.5);
+
+  const double epsilon = 0.01;
+
+  InfiniteVector<double, Index> u_epsilon;
+
+  clock_t tstart, tend;
+  double time;
+  tstart = clock();
+
+  steepest_descent_SOLVE(problem, epsilon, u_epsilon);
+ //  for (unsigned int i = 0; i < 50*20;i++)
+//     for (Index ind = FrameTL::first_wavelet<Basis1D,1,1,Frame1D>(&frame, frame.j0());
+// 	 ind <= FrameTL::last_wavelet<Basis1D,1,1,Frame1D>(&frame, frame.j0()+2); ++ind)
+//       {
+// 	;
+//       }
+  tend = clock();
+  time = (double)(tend-tstart)/CLOCKS_PER_SEC;
+  cout << "  ... done, time needed: " << time << " seconds" << endl;
+
+  cout << "steepest descent done" << endl;
+
+  //discrete_poisson.rescale(u_epsilon,-1);
+  problem.rescale(u_epsilon,-1);
+
+  Array1D<SampledMapping<2> > U = evalObj.evaluate(frame, u_epsilon, true, 6);//expand in primal basis
+
+  Array1D<SampledMapping<2> > Error = evalObj.evaluate_difference(frame, u_epsilon, sing2D, 6);
+
+  std::ofstream ofs5("approx_sol_steep_2D_out.m");
+  matlab_output(ofs5,U);
+  ofs5.close();
+
+  std::ofstream ofs6("error_steep_2D_out.m");
+  matlab_output(ofs6,Error);
+  ofs6.close();
+
+
+
+  return 0;
+
+}

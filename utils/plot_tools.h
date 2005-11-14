c1c9c79e1e29c392a8f5bfd1d009cac3532ad69e
werner <werner>
2005-11-14 17:53:28 +0000

    added plot_tools.h

---
+++ utils/plot_tools.h
@@ -0,0 +1,40 @@
+// -*- c++ -*-
+
+// +--------------------------------------------------------------------+
+// | This file is part of MathTL - the Mathematical Template Library    |
+// |                                                                    |
+// | Copyright (c) 2002-2005                                            |
+// | Thorsten Raasch, Manuel Werner                                     |
+// +--------------------------------------------------------------------+
+
+#ifndef _MATHTL_PLOT_TOOLS_H
+#define _MATHTL_PLOT_TOOLS_H
+
+#include<map>
+#include<geometry/sampled_mapping.h>
+
+using std::map;
+
+namespace MathTL {
+
+  void matlab_output(const map<double,double>& log_10_residual_norms,
+		     std::ostream& os)
+  {
+    Array1D<double> x(log_10_residual_norms.size());
+    Array1D<double> values(log_10_residual_norms.size());
+    
+    map<double,double>::const_iterator it = log_10_residual_norms.begin();
+    unsigned int i = 0;
+    for (; it != log_10_residual_norms.end(); it++) {
+      x[i] = it->first;
+      values[i] = it->second;
+      i++;
+    }
+
+    Grid<1> grid(x);
+    SampledMapping<1> s_out(grid,values);
+    s_out.matlab_output(os);
+    
+  }
+}
+#endif

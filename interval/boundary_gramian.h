0172ff2a31abff02d2330a088c8b2af0642a484b
raasch <raasch>
2006-03-02 17:15:07 +0000

    nothing spectatular, finished the algebraic comment

--- interval/boundary_gramian.h
+++ interval/boundary_gramian.h
@@ -21,10 +21,7 @@ namespace WaveletTL
   /*!
     Assume that phi, phiT are refinable functions on R,
     with masks MASK1/MASK2 and with compact supports [l1,l2]\subset [l1T,l2T].
-    It is known that the inner products <phi(.-k),phiT(.-l)> over R can be
-    computed exactly, since they correspond to the point values at the integers
-    of a special compactly supported refinable function, see the
-    template class DMMask for their refinement coefficients.
+    Moreover, in this routine, phi and phiT are assumed to be biorthogonal.
 
     The following routine computes the n-times-n gramian matrix
       Gamma_L = <phi^L_{0,k},phiT^L_{0,l}>_{k,l=-l1T-m-n,...,-l1T-m-1}
@@ -42,9 +39,9 @@ namespace WaveletTL
   */
   template <class MASK1, class MASK2>
   void
-  compute_boundary_gramian(const Matrix<double>& ML,
-			   const Matrix<double>& MLT,
-			   Matrix<double>& GammaL);
+  compute_biorthogonal_boundary_gramian(const Matrix<double>& ML,
+					const Matrix<double>& MLT,
+					Matrix<double>& GammaL);
 }
 
 #include <interval/boundary_gramian.cpp>

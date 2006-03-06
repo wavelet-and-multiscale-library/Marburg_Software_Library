6497325525872f0f3f0f474078eb85f601f29108
raasch <raasch>
2006-03-06 18:43:36 +0000

    inserted and removed two cout calls

--- numerics/splines.cpp
+++ numerics/splines.cpp
@@ -16,7 +16,7 @@ namespace MathTL
     for (int n = 0; n <= d-2-k0; n++)
       for (int k = 0; k < -k0; k++)
 	B1.set_entry(n, k, evaluate_Bspline<d>(knots, k0+k, n/2.));
-
+    
     UpperTriangularMatrix<double> B2(d-1-k0, d-1-k0);
     for (int k = 0; k <= d-2-k0; k++)
       for (int n = k; n <= d-2-k0; n++)

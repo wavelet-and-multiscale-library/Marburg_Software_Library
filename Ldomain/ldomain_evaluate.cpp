e2ac6c62b45b02bafd00286c341074c825e5d21c
raasch <raasch>
2006-04-11 09:51:37 +0000

    Matlab evaluation of LDomainBasis generators

--- Ldomain/ldomain_evaluate.cpp
+++ Ldomain/ldomain_evaluate.cpp
@@ -11,15 +11,179 @@ namespace WaveletTL
   {
     Array1D<SampledMapping<2> > r(3);
 
-    Grid<2> grid0(Point<2>(0), Point<2>(1), 1<<resolution);
-    FixedArray1D<Array1D<double>,2> values;
-    values[0].resize((1<<resolution)+1);
-    values[1].resize((1<<resolution)+1);
-    
-    r[0] = SampledMapping<2>(Point<2>(-1, 0), Point<2>(0,1), values);
-    r[1] = SampledMapping<2>(Point<2>(-1,-1), Point<2>(0,0), values);
-    r[2] = SampledMapping<2>(Point<2>( 0,-1), Point<2>(1,0), values);
+    typedef typename LDomainBasis<IBASIS>::Index Index;
 
+    typename Index::type_type zero;
+    if (lambda.e() == zero) {
+      // psi_lambda is a generator. Here we know that per patch,
+      // we can resort to a single tensor product.
+      
+      FixedArray1D<Array1D<double>,2> values;
+      values[0].resize((1<<resolution)+1); // values in x-direction
+      values[1].resize((1<<resolution)+1); // values in y-direction
+
+      // patch 0
+      switch (lambda.p()) {
+      case 0:
+	// psi_lambda completely lives on patch 0
+	values[0] = evaluate(basis.basis00(),
+			     typename IBASIS::Index(lambda.j(),
+						    0,
+						    lambda.k()[0],
+						    &basis.basis00()),
+			     primal,
+			     resolution).values();
+	values[1] = evaluate(basis.basis10(),
+			     typename IBASIS::Index(lambda.j(),
+						    0,
+						    lambda.k()[1],
+						    &basis.basis10()),
+			     primal,
+			     resolution).values();
+	r[0] = SampledMapping<2>(Point<2>(-1, 0), Point<2>(0,1), values);
+	
+	for (int i = 0; i <= 1<<resolution; i++) {
+	  values[0][i] = values[1][i] = 0;
+	}
+	r[1] = SampledMapping<2>(Point<2>(-1,-1), Point<2>(0,0), values);
+	r[2] = SampledMapping<2>(Point<2>( 0,-1), Point<2>(1,0), values);
+	break;
+      case 1:
+	// psi_lambda completely lives on patch 1
+	values[0] = evaluate(basis.basis01(),
+			     typename IBASIS::Index(lambda.j(),
+						    0,
+						    lambda.k()[0],
+						    &basis.basis01()),
+			     primal,
+			     resolution).values();
+	values[1] = evaluate(basis.basis01(),
+			     typename IBASIS::Index(lambda.j(),
+						    0,
+						    lambda.k()[1],
+						    &basis.basis01()),
+			     primal,
+			     resolution).values();
+	r[1] = SampledMapping<2>(Point<2>(-1,-1), Point<2>(0,0), values);
+
+	for (int i = 0; i <= 1<<resolution; i++) {
+	  values[0][i] = values[1][i] = 0;
+	}
+	r[0] = SampledMapping<2>(Point<2>(-1, 0), Point<2>(0,1), values);
+	r[2] = SampledMapping<2>(Point<2>( 0,-1), Point<2>(1,0), values);
+	break;
+      case 2:
+	// psi_lambda completely lives on patch 2
+	values[0] = evaluate(basis.basis10(),
+			     typename IBASIS::Index(lambda.j(),
+						    0,
+						    lambda.k()[0],
+						    &basis.basis10()),
+			     primal,
+			     resolution).values();
+	values[1] = evaluate(basis.basis00(),
+			     typename IBASIS::Index(lambda.j(),
+						    0,
+						    lambda.k()[1],
+						    &basis.basis00()),
+			     primal,
+			     resolution).values();
+	r[2] = SampledMapping<2>(Point<2>( 0,-1), Point<2>(1,0), values);
+
+	for (int i = 0; i <= 1<<resolution; i++) {
+	  values[0][i] = values[1][i] = 0;
+	}
+	r[0] = SampledMapping<2>(Point<2>(-1, 0), Point<2>(0,1), values);
+	r[1] = SampledMapping<2>(Point<2>(-1,-1), Point<2>(0,0), values);
+	break;
+      case 3:
+	// psi_lambda lives on patches 0 and 1
+	values[0] = evaluate(basis.basis00(),
+			     typename IBASIS::Index(lambda.j(),
+						    0,
+						    lambda.k()[0],
+						    &basis.basis00()),
+			     primal,
+			     resolution).values();
+	values[1] = evaluate(basis.basis10(),
+			     typename IBASIS::Index(lambda.j(),
+						    0,
+						    basis.basis10().DeltaLmin(),
+						    &basis.basis10()),
+			     primal,
+			     resolution).values();
+	for (int i = 0; i <= 1<<resolution; i++) values[1][i] *= M_SQRT1_2;
+	r[0] = SampledMapping<2>(Point<2>(-1, 0), Point<2>(0,1), values);
+
+	values[0] = evaluate(basis.basis01(),
+			     typename IBASIS::Index(lambda.j(),
+						    0,
+						    lambda.k()[0],
+						    &basis.basis01()),
+			     primal,
+			     resolution).values();
+	values[1] = evaluate(basis.basis01(),
+			     typename IBASIS::Index(lambda.j(),
+						    0,
+						    basis.basis01().DeltaRmax(lambda.j()),
+						    &basis.basis01()),
+			     primal,
+			     resolution).values();
+	for (int i = 0; i <= 1<<resolution; i++) values[1][i] *= M_SQRT1_2;
+	r[1] = SampledMapping<2>(Point<2>(-1,-1), Point<2>(0,0), values);
+
+	for (int i = 0; i <= 1<<resolution; i++) {
+	  values[0][i] = values[1][i] = 0;
+	}
+	r[2] = SampledMapping<2>(Point<2>( 0,-1), Point<2>(1,0), values);
+	break;
+      case 4:
+	// psi_lambda lives on patches 1 and 2
+	values[0] = evaluate(basis.basis01(),
+			     typename IBASIS::Index(lambda.j(),
+						    0,
+						    basis.basis01().DeltaRmax(lambda.j()),
+						    &basis.basis01()),
+			     primal,
+			     resolution).values();
+	values[1] = evaluate(basis.basis01(),
+			     typename IBASIS::Index(lambda.j(),
+						    0,
+						    lambda.k()[1],
+						    &basis.basis01()),
+			     primal,
+			     resolution).values();
+	for (int i = 0; i <= 1<<resolution; i++) values[1][i] *= M_SQRT1_2;
+	r[1] = SampledMapping<2>(Point<2>(-1,-1), Point<2>(0,0), values);
+
+	values[0] = evaluate(basis.basis10(),
+			     typename IBASIS::Index(lambda.j(),
+						    0,
+						    basis.basis10().DeltaLmin(),
+						    &basis.basis10()),
+			     primal,
+			     resolution).values();
+	values[1] = evaluate(basis.basis00(),
+			     typename IBASIS::Index(lambda.j(),
+						    0,
+						    lambda.k()[1],
+						    &basis.basis00()),
+			     primal,
+			     resolution).values();
+	for (int i = 0; i <= 1<<resolution; i++) values[1][i] *= M_SQRT1_2;
+	r[2] = SampledMapping<2>(Point<2>( 0,-1), Point<2>(1,0), values);
+
+	for (int i = 0; i <= 1<<resolution; i++) {
+	  values[0][i] = values[1][i] = 0;
+	}
+	r[0] = SampledMapping<2>(Point<2>(-1, 0), Point<2>(0,1), values);
+	break;
+      }
+    } else {
+      // psi_lambda is a wavelet. Here the situation is more complicated
+      // due to the nonlocal biorthogonalization procedure.
+    }
+    
     return r;
   }
     
--- tests/test_ldomain_basis.cpp
+++ tests/test_ldomain_basis.cpp
@@ -3,6 +3,7 @@
 #include <algebra/infinite_vector.h>
 
 #include <interval/ds_basis.h>
+#include <interval/p_basis.h>
 #include <Ldomain/ldomain_basis.h>
 
 using namespace std;
@@ -15,19 +16,29 @@ int main()
   const int d  = 2;
   const int dT = 2;
 
-  typedef DSBasis<d,dT> Basis1D;
+//   typedef DSBasis<d,dT> Basis1D; // remember to set partialSVD/BernsteinSVD biorthogonalization here!
+  typedef PBasis<d,dT> Basis1D;
   typedef LDomainBasis<Basis1D> Basis;
   Basis basis;
 
   typedef Basis::Index Index;
 
+  cout << "- j0=" << basis.j0() << endl;
+  cout << "- the default wavelet index: " << Index() << endl;
+  cout << "- the default wavelet index w.r.t. the cube basis: " << Index(&basis) << endl;
+  cout << "- first generator on the coarsest level: " << first_generator<Basis1D>(&basis, basis.j0()) << endl;
+  cout << "- last generator on the coarsest level: " << last_generator<Basis1D>(&basis, basis.j0()) << endl;
+  cout << "- first wavelet on the coarsest level: " << first_wavelet<Basis1D>(&basis, basis.j0()) << endl;
+  cout << "- last wavelet on the coarsest level: " << last_wavelet<Basis1D>(&basis, basis.j0()) << endl;
+
 #if 1
-  cout << "- evaluating a primal generator..." << endl;
   Index lambda(first_generator<Basis1D>(&basis, basis.j0()));
+  for (int i = 0; i < 150; i++, ++lambda);
+  cout << "- evaluating a primal generator lambda=" << lambda << " ..." << endl;
   std::ofstream psistream("Ldomain_wavelet.m");
   matlab_output(psistream, evaluate<Basis1D>(basis, lambda, true, 6));
   psistream.close();
-  cout << "  ...done, see file cube_wavelet.m!" << endl;
+  cout << "  ...done, see file Ldomain_wavelet.m!" << endl;
 #endif
 
 }

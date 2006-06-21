87d6df743c7eae90bfda9430224fd3f5f92f0dbc
raasch <raasch>
2006-06-21 14:31:27 +0000

    speedup in KroneckerMatrix::apply(), ::apply_transposed() by resorting only to the apply() and apply_transposed() routines of the Kronecker factors (rather than accessing the entries directly)

--- algebra/kronecker_matrix.cpp
+++ algebra/kronecker_matrix.cpp
@@ -1,6 +1,7 @@
 // implementation for kronecker_matrix.h
 
 #include <iomanip>
+#include <utils/array1d.h>
 
 namespace MathTL
 {
@@ -83,6 +84,51 @@ namespace MathTL
   {
     assert(Mx.size() == row_dimension());
     
+#if 1
+    // new variant, using only the apply() methods
+
+    // for readability:
+    const size_type m = A.row_dimension();
+    const size_type n = A.column_dimension();
+    const size_type p = B.row_dimension();
+    const size_type q = B.column_dimension();
+
+    // first decompose x into the corresponding blocks
+    Array1D<Vector<C> > x_blocks(n);
+    for (size_type j = 0; j < n; j++) {
+      x_blocks[j].resize(q);
+      for (size_type l = 0; l < q; l++)
+	x_blocks[j][l] = x[j*q+l];
+    }
+
+    // apply B to these blocks
+    Array1D<Vector<C> > Bx_blocks(n);
+    for (size_type j = 0; j < n; j++) {
+      Bx_blocks[j].resize(p);
+      B.apply(x_blocks[j], Bx_blocks[j]);
+    }
+    
+    // resort the Bx[i] blocks
+    Array1D<Vector<C> > w_blocks(p);
+    for (size_type k = 0; k < p; k++) {
+      w_blocks[k].resize(n);
+      for (size_type j = 0; j < n; j++)
+	w_blocks[k][j] = Bx_blocks[j][k];
+    }
+    
+    // apply A to the w[k] blocks
+    Array1D<Vector<C> > Aw_blocks(p);
+    for (size_type k = 0; k < p; k++) {
+      Aw_blocks[k].resize(m);
+      A.apply(w_blocks[k], Aw_blocks[k]);
+    }
+    
+    // resort the Aw[k] blocks into the result
+    for (size_type i = 0; i < m; i++)
+      for (size_type k = 0; k < p; k++)
+	Mx[i*p+k] = Aw_blocks[k][i];
+#else
+    // old code, resorting to single entries
     for (size_type Arow(0), Mxrow(0); Arow < A.row_dimension(); Arow++)
       for (size_type Brow(0); Brow < B.row_dimension(); Brow++, Mxrow++) {
 	C help(0);
@@ -99,6 +145,7 @@ namespace MathTL
 	
 	Mx[Mxrow] = help;
       }
+#endif
   }
   
   template <class C, class MATRIX1, class MATRIX2>
@@ -107,6 +154,51 @@ namespace MathTL
   {
     assert(Mx.size() == row_dimension());
 
+#if 1
+    // new variant, using only the apply() methods
+
+    // for readability:
+    const size_type m = A.row_dimension();
+    const size_type n = A.column_dimension();
+    const size_type p = B.row_dimension();
+    const size_type q = B.column_dimension();
+
+    // first decompose x into the corresponding blocks
+    Array1D<Vector<C> > x_blocks(n);
+    for (size_type j = 0; j < n; j++) {
+      x_blocks[j].resize(q);
+      for (size_type l = 0; l < q; l++)
+	x_blocks[j][l] = x[j*q+l];
+    }
+
+    // apply B to these blocks
+    Array1D<Vector<C> > Bx_blocks(n);
+    for (size_type j = 0; j < n; j++) {
+      Bx_blocks[j].resize(p);
+      B.apply(x_blocks[j], Bx_blocks[j]);
+    }
+    
+    // resort the Bx[i] blocks
+    Array1D<Vector<C> > w_blocks(p);
+    for (size_type k = 0; k < p; k++) {
+      w_blocks[k].resize(n);
+      for (size_type j = 0; j < n; j++)
+	w_blocks[k][j] = Bx_blocks[j][k];
+    }
+    
+    // apply A to the w[k] blocks
+    Array1D<Vector<C> > Aw_blocks(p);
+    for (size_type k = 0; k < p; k++) {
+      Aw_blocks[k].resize(m);
+      A.apply(w_blocks[k], Aw_blocks[k]);
+    }
+    
+    // resort the Aw[k] blocks into the result
+    for (size_type i = 0; i < m; i++)
+      for (size_type k = 0; k < p; k++)
+	Mx[i*p+k] = Aw_blocks[k][i];
+#else
+    // old code, resorting to single entries
     for (size_type Arow(0), Mxrow(0); Arow < A.row_dimension(); Arow++)
       for (size_type Brow(0); Brow < B.row_dimension(); Brow++, Mxrow++) {
 	C help(0);
@@ -123,6 +215,7 @@ namespace MathTL
 	
 	Mx[Mxrow] = help;
       }
+#endif
   }
   
   template <class C, class MATRIX1, class MATRIX2>
@@ -132,6 +225,51 @@ namespace MathTL
   {
     assert(Mtx.size() == column_dimension());
     
+#if 1
+    // new variant, using only the apply() methods
+
+    // for readability:
+    const size_type m = A.row_dimension();
+    const size_type n = A.column_dimension();
+    const size_type p = B.row_dimension();
+    const size_type q = B.column_dimension();
+
+    // first decompose x into the corresponding blocks
+    Array1D<Vector<C> > x_blocks(m);
+    for (size_type i = 0; i < m; i++) {
+      x_blocks[i].resize(p);
+      for (size_type k = 0; k < p; k++)
+	x_blocks[i][k] = x[i*p+k];
+    }
+
+    // apply B^T to these blocks
+    Array1D<Vector<C> > BTx_blocks(m);
+    for (size_type i = 0; i < m; i++) {
+      BTx_blocks[i].resize(q);
+      B.apply_transposed(x_blocks[i], BTx_blocks[i]);
+    }
+    
+    // resort the BTx[i] blocks
+    Array1D<Vector<C> > w_blocks(q);
+    for (size_type l = 0; l < q; l++) {
+      w_blocks[l].resize(m);
+      for (size_type i = 0; i < m; i++)
+ 	w_blocks[l][i] = BTx_blocks[i][l];
+    }
+    
+    // apply A^T to the w[l] blocks
+    Array1D<Vector<C> > ATw_blocks(q);
+    for (size_type l = 0; l < q; l++) {
+      ATw_blocks[l].resize(n);
+      A.apply_transposed(w_blocks[l], ATw_blocks[l]);
+    }
+    
+    // resort the ATw[l] blocks into the result
+    for (size_type j = 0; j < n; j++)
+      for (size_type l = 0; l < q; l++)
+ 	Mtx[j*q+l] = ATw_blocks[l][j];
+#else
+    // old code, resorting to single entries
     for (size_type Acol(0), Mtxrow(0); Acol < A.column_dimension(); Acol++)
       for (size_type Bcol(0); Bcol < B.column_dimension(); Bcol++, Mtxrow++) {
 	C help(0);
@@ -148,6 +286,7 @@ namespace MathTL
 	
 	Mtx[Mtxrow] = help;
       }
+#endif
   }
   
   template <class C, class MATRIX1, class MATRIX2>
@@ -156,6 +295,51 @@ namespace MathTL
   {
     assert(Mtx.size() == column_dimension());
     
+#if 1
+    // new variant, using only the apply() methods
+
+    // for readability:
+    const size_type m = A.row_dimension();
+    const size_type n = A.column_dimension();
+    const size_type p = B.row_dimension();
+    const size_type q = B.column_dimension();
+
+    // first decompose x into the corresponding blocks
+    Array1D<Vector<C> > x_blocks(m);
+    for (size_type i = 0; i < m; i++) {
+      x_blocks[i].resize(p);
+      for (size_type k = 0; k < p; k++)
+	x_blocks[i][k] = x[i*p+k];
+    }
+
+    // apply B^T to these blocks
+    Array1D<Vector<C> > BTx_blocks(m);
+    for (size_type i = 0; i < m; i++) {
+      BTx_blocks[i].resize(q);
+      B.apply_transposed(x_blocks[i], BTx_blocks[i]);
+    }
+    
+    // resort the BTx[i] blocks
+    Array1D<Vector<C> > w_blocks(q);
+    for (size_type l = 0; l < q; l++) {
+      w_blocks[l].resize(m);
+      for (size_type i = 0; i < m; i++)
+ 	w_blocks[l][i] = BTx_blocks[i][l];
+    }
+    
+    // apply A^T to the w[l] blocks
+    Array1D<Vector<C> > ATw_blocks(q);
+    for (size_type l = 0; l < q; l++) {
+      ATw_blocks[l].resize(n);
+      A.apply_transposed(w_blocks[l], ATw_blocks[l]);
+    }
+    
+    // resort the ATw[l] blocks into the result
+    for (size_type j = 0; j < n; j++)
+      for (size_type l = 0; l < q; l++)
+ 	Mtx[j*q+l] = ATw_blocks[l][j];
+#else
+    // old code, resorting to single entries
     for (size_type Acol(0), Mtxrow(0); Acol < A.column_dimension(); Acol++)
       for (size_type Bcol(0); Bcol < B.column_dimension(); Bcol++, Mtxrow++) {
 	C help(0);
@@ -172,6 +356,7 @@ namespace MathTL
 	
 	Mtx[Mtxrow] = help;
       }
+#endif
   }
   
   template <class C, class MATRIX1, class MATRIX2>

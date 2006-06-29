8fa787a395cdaf4bc0d47c948f7850b8ad760ff1
raasch <raasch>
2006-06-29 16:22:28 +0000

    added point evaluation of cubic polynomials in (Bezier-)Bernstein representation

--- numerics/bezier.h
+++ numerics/bezier.h
@@ -13,7 +13,7 @@
 namespace MathTL
 {
   /*!
-    point evaluation of the k-th Bernstein polynomial of degree d
+    point evaluation of the k-th Bernstein basis polynomial of degree d
 
       B_{k,d}(x) = binomial{d}{k} * x^k * (1-x)^{d-k}
 
@@ -34,6 +34,35 @@ namespace MathTL
   double EvaluateBernsteinPolynomial<0>(const int k, const double x)
   {
     return (k == 0 ? 1.0 : 0.0);
+  }
+
+  /*!
+    point evaluation of the first derivative of B_{k,d}(x)
+  */
+  template <int d>
+  double EvaluateBernsteinPolynomial_x(const int k, const double x)
+  {
+    return d * (EvaluateBernsteinPolynomial<d-1>(k-1, x)
+		- EvaluateBernsteinPolynomial<d-1>(k, x));
+  }
+
+  /*!
+    point evaluation of a cubic polynomial p(x) in Bernstein representation
+     
+      p(x) = sum_{k=0}^3 B_{k,3}(x) * b_k
+  */
+  double EvaluateBernsteinPolynomial(const double b0, const double b1,
+				     const double b2, const double b3,
+				     const double x)
+  {
+    const double b10 = (1-x)*b0+x*b1;
+    const double b11 = (1-x)*b1+x*b2;
+    const double b12 = (1-x)*b2+x*b3;
+    
+    const double b20 = (1-x)*b10+x*b11;
+    const double b21 = (1-x)*b11+x*b12;
+
+    return (1-x)*b20+x*b21;
   }
 }
 
--- tests/test_bezier.cpp
+++ tests/test_bezier.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <numerics/bezier.h>
+#include <utils/array1d.h>
 
 using std::cout;
 using std::endl;
@@ -10,14 +11,52 @@ int main()
 {
   cout << "Testing Bezier stuff from MathTL:: ..." << endl;
 
+#if 0
   const int d = 1;
   for (int k = -1; k <= d+1; k++) {
     cout << "* some point values of the " << k << "-th Bernstein polynomial of degree " << d << ":" << endl;
-    for (double x = 0.0; x <= 1.0; x+=0.1) {
-      cout << "  B_{" << k << "," << d << "}(" << x << ")="
-	   <<  EvaluateBernsteinPolynomial<d>(k, x) << endl;
+    const unsigned int n = 20;
+    Array1D<double> xi(n+1), yi(n+1);
+    for (unsigned int i = 0; i <= n; i++) {
+      xi[i] = i/(double)n;
+      yi[i] = EvaluateBernsteinPolynomial<d>(k, xi[i]);
     }
+    
+    cout << "  xi=" << xi << endl;
+    cout << "  yi=" << yi << endl;
   }
+#endif
+#if 0
+  for (int k = -1; k <= d+1; k++) {
+    cout << "* some point values of the 1st derivative of the " << k << "-th Bernstein polynomial of degree " << d << ":" << endl;
+    const unsigned int n = 20;
+    Array1D<double> xi(n+1), yi(n+1);
+    for (unsigned int i = 0; i <= n; i++) {
+      xi[i] = i/(double)n;
+      yi[i] = EvaluateBernsteinPolynomial_x<d>(k, xi[i]);
+    }
+    
+    cout << "  xi=" << xi << endl;
+    cout << "  yi=" << yi << endl;
+  }
+#endif
+  
+  double b0 = 0;
+  double b1 = 1;
+  double b2 = 1;
+  double b3 = 1;
+  cout << "* some point values from a cubic Bernstein polynomial with Bezier coefficients "
+       << "b0=" << b0 << ", b1=" << b1 << ", b2=" << b2 << ", b3=" << b3 << ":" << endl;
+
+  const unsigned int n = 20;
+  Array1D<double> xi(n+1), yi(n+1);
+  for (unsigned int i = 0; i <= n; i++) {
+    xi[i] = i/(double)n;
+    yi[i] = EvaluateBernsteinPolynomial(b0, b1, b2, b3, xi[i]);
+  }
+
+  cout << "  xi=" << xi << endl;
+  cout << "  yi=" << yi << endl;
   
   return 0;
 }

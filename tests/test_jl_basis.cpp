dc2ca4c45f441bd9f6404a5455bbbfc8812678fa
raasch <raasch>
2006-06-30 08:54:43 +0000

    basic index functionality of JLBasis

--- tests/test_jl_basis.cpp
+++ tests/test_jl_basis.cpp
@@ -14,10 +14,10 @@ int main()
   Basis basis;
 
   cout << "- the default wavelet index: " << Index() << endl;
-//   cout << "- leftmost generator on the coarsest level: " << first_generator(&basis, basis.j0()) << endl;
-//   cout << "- rightmost generator on the coarsest level: " << last_generator(&basis, basis.j0()) << endl;
-//   cout << "- leftmost wavelet on the coarsest level: " << first_wavelet(&basis, basis.j0()) << endl;
-//   cout << "- rightmost wavelet on the coarsest level: " << last_wavelet(&basis, basis.j0()) << endl;
+  cout << "- leftmost generator on the coarsest level: " << first_generator(&basis, basis.j0()) << endl;
+  cout << "- rightmost generator on the coarsest level: " << last_generator(&basis, basis.j0()) << endl;
+  cout << "- leftmost wavelet on the coarsest level: " << first_wavelet(&basis, basis.j0()) << endl;
+  cout << "- rightmost wavelet on the coarsest level: " << last_wavelet(&basis, basis.j0()) << endl;
 
   return 0;
 }

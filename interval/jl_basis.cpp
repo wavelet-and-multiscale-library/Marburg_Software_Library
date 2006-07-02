372203b61bc2baac630ad996822e63c22d899331
raasch <raasch>
2006-07-02 23:00:42 +0000

    JLBasis::reconstruct_1() works for primal wavelets (not yet for generators)

--- interval/jl_basis.cpp
+++ interval/jl_basis.cpp
@@ -1,5 +1,7 @@
 // implementation for jl_basis.h
 
+#include <cmath>
+
 namespace WaveletTL
 {
   JLBasis::JLBasis(const int s0, const int s1)
@@ -37,9 +39,79 @@ namespace WaveletTL
     if (lambda.j() >= j)
       c.set_coefficient(lambda, 1.0); 
     else {
-      // For the reconstruction of psi_lambda, we have to compute
-      // the corresponding column of the transformation matrix Mj=(Mj0, Mj1).
-      
+      if (lambda.e() == 0) {
+	// generator
+
+	const int first_half = (1<<lambda.j())-get_s1();
+	if (lambda.k() <= first_half) {
+	  // type phi_0
+	  // phi_0(x) = 1/2*phi_0(2*x+1)+phi_0(2*x)+1/2*phi_0(2*x-1)+3/4*phi_1(2*x+1)-3/4*phi_1(2*x-1)
+
+	  // TODO!!!
+	} else {
+	  // type phi_1
+	  // phi_1(x) = -1/8*phi_0(2*x+1)+1/8*phi_0(2*x-1)-1/8*phi_1(2*x+1)+1/2*phi_1(2*x)-1/8*phi_1(2*x+1)
+
+	  // TODO!!!
+	}
+      } else {
+	// wavelet
+	const int first_half = (1<<lambda.j())-1;
+	if (lambda.k() <= first_half) {
+	  // type psi_0
+	  // psi_0(x) = -2*phi_0(2*x+1)+4*phi_0(2*x)-2*phi_0(2*x-1)-21*phi_1(2*x+1)+21*phi_1(2*x-1)
+	  const int offset = (1<<(lambda.j()+1))-get_s1()+1;
+	  for (int m = 2*lambda.k()-1; m <= 2*lambda.k()+1; m++) {
+	    switch (m-2*lambda.k()) {
+	    case -1:
+	      if (m >= get_s0() && m < offset)
+		c.add_coefficient(Index(lambda.j()+1, 0, m, this), -2.0*M_SQRT1_2);
+	      if (m >= 0 && m+offset <= DeltaRmax(lambda.j()+1))
+		c.add_coefficient(Index(lambda.j()+1, 0, offset+m, this), -21.0*M_SQRT1_2);
+	      break;
+	    case 0:
+	      if (m >= get_s0() && m < offset)
+		c.add_coefficient(Index(lambda.j()+1, 0, m, this), 4.0*M_SQRT1_2);
+	      break;
+	    case 1:
+	      if (m >= get_s0() && m < offset)
+		c.add_coefficient(Index(lambda.j()+1, 0, m, this), -2.0*M_SQRT1_2);
+	      if (m >= 0 && m+offset <= DeltaRmax(lambda.j()+1))
+		c.add_coefficient(Index(lambda.j()+1, 0, offset+m, this), 21.0*M_SQRT1_2);
+	      break;
+	    default:
+	      break;
+	    }
+	  }
+	} else {
+	  // type psi_1
+	  // psi_1(x) = phi_0(2*x+1)-phi_0(2*x-1)+ 9*phi_1(2*x+1)+12*phi_1(2*x)+ 9*phi_1(2*x-1)
+	  const int offset = (1<<(lambda.j()+1))-get_s1()+1;
+	  const int lambdak = lambda.k()-first_half-1;
+	  for (int m = 2*lambdak-1; m <= 2*lambdak+1; m++) {
+	    switch (m-2*lambdak) {
+	    case -1:
+	      if (m >= get_s0() && m < offset)
+		c.add_coefficient(Index(lambda.j()+1, 0, m, this), M_SQRT1_2);
+	      if (m >= 0 && m+offset <= DeltaRmax(lambda.j()+1))
+		c.add_coefficient(Index(lambda.j()+1, 0, offset+m, this), 9.0*M_SQRT1_2);
+	      break;
+	    case 0:
+	      if (m >= 0 && m+offset <= DeltaRmax(lambda.j()+1))
+		c.add_coefficient(Index(lambda.j()+1, 0, offset+m, this), 12.0*M_SQRT1_2);
+	      break;
+	    case 1:
+	      if (m >= get_s0() && m < offset)
+		c.add_coefficient(Index(lambda.j()+1, 0, m, this), -M_SQRT1_2);
+	      if (m >= 0 && m+offset <= DeltaRmax(lambda.j()+1))
+		c.add_coefficient(Index(lambda.j()+1, 0, offset+m, this), 9.0*M_SQRT1_2);
+	      break;
+	    default:
+	      break;
+	    }
+	  }
+	}
+      }
     }
   }
   
--- tests/test_jl_basis.cpp
+++ tests/test_jl_basis.cpp
@@ -19,6 +19,19 @@ int main()
   cout << "- rightmost generator on the coarsest level: " << last_generator(&basis, basis.j0()) << endl;
   cout << "- leftmost wavelet on the coarsest level: " << first_wavelet(&basis, basis.j0()) << endl;
   cout << "- rightmost wavelet on the coarsest level: " << last_wavelet(&basis, basis.j0()) << endl;
+  int testlevel = 2;
+  cout << "- leftmost generator on the level " << testlevel << ": " << first_generator(&basis, testlevel) << endl;
+  cout << "- rightmost generator on the level " << testlevel << ": " << last_generator(&basis, testlevel) << endl;
+  cout << "- leftmost wavelet on the level " << testlevel << ": " << first_wavelet(&basis, testlevel) << endl;
+  cout << "- rightmost wavelet on the level " << testlevel << ": " << last_wavelet(&basis, testlevel) << endl;
+
+//   Index mu(2,1,3,&basis);
+  Index mu(first_wavelet(&basis, basis.j0()));
+  cout << "* a wavelet coefficient mu=" << mu << endl;
+  cout << "  - reconstruct_1() yields" << endl;
+  InfiniteVector<double,Index> gcoeffs;
+  basis.reconstruct_1(mu,mu.j()+1,gcoeffs);
+  cout << gcoeffs;
 
 #if 1
   cout << "- evaluating some primal generators:" << endl;

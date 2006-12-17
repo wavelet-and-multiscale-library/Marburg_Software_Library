556c858fa7f90e2a1990e8a94bf6f799795a8a2c
raasch <raasch>
2006-12-17 15:48:25 +0000

    added class JLIndex

---
+++ interval/jl_index.cpp
@@ -0,0 +1,113 @@
+// implementation for jl_index.h
+
+namespace WaveletTL
+{
+  JLIndex::JLIndex()
+    : j_(1), e_(0), c_(0), k_(1)
+  {
+  }
+
+  JLIndex::JLIndex(const int j, const type_type e, const component_type c, const translation_type k)
+    : j_(j), e_(e), c_(c), k_(k)
+  {
+  }
+  
+  JLIndex&
+  JLIndex::operator = (const JLIndex& lambda)
+  {
+    j_ = lambda.j();
+    e_ = lambda.e();
+    c_ = lambda.c();
+    k_ = lambda.k();
+
+    return *this;
+  }
+
+  bool
+  JLIndex::operator == (const JLIndex& lambda) const
+  {
+    return (j_ == lambda.j() &&
+	    e_ == lambda.e() &&
+	    c_ == lambda.c() &&
+	    k_ == lambda.k());
+  }
+  
+  bool
+  JLIndex::operator < (const JLIndex& lambda) const
+  {
+    return (j_ < lambda.j() ||
+	    (j_ == lambda.j() && (e_ < lambda.e() ||
+				  (e_ == lambda.e() && (c_ < lambda.c() ||
+							(c_ == lambda.c() && k_< lambda.k()))))));
+  }
+  
+  JLIndex&
+  JLIndex::operator ++ ()
+  {
+    if (e_==0) {
+      if (c_==0) {
+	if (k_==(1<<j_)-1) {
+	  c_ = 1;
+	  k_ = 0;
+	}
+	else
+	  k_++;
+      } else {
+	// c_==1
+	if (k_==1<<j_) {
+	  e_ = 1;
+	  c_ = 0;
+	  k_ = 1;
+	}
+	else
+	  k_++;
+      }
+    } else {
+      // e_==1
+      if (c_==0) {
+	if (k_==(1<<j_)-1) {
+	  c_ = 1;
+	  k_ = 0;
+	}
+	else
+	  k_++;
+      } else {
+	// c_==1
+	if (k_==1<<j_) {
+	  j_++;
+	  c_=0;
+	  k_=1;
+	}
+	else
+	  k_++;
+      }
+    }
+    
+    return *this;
+  }
+  
+  JLIndex first_generator(const int j)
+  {
+    assert(j >= 1);
+    return JLIndex(j, 0, 0, 1);
+  }
+  
+  JLIndex last_generator(const int j)
+  {
+    assert(j >= 1);
+    return JLIndex(j, 0, 1, 1<<j);
+  }
+  
+  JLIndex first_wavelet(const int j)
+  {
+    assert(j >= 1);
+    return JLIndex(j, 1, 0, 1);
+  }
+  
+  JLIndex last_wavelet(const int j)
+  {
+    assert(j >= 1);
+    return JLIndex(j, 1, 1, 1<<j);
+  }
+
+}
---
+++ interval/jl_index.h
@@ -0,0 +1,122 @@
+// -*- c++ -*-
+
+// +--------------------------------------------------------------------+
+// | This file is part of WaveletTL - the Wavelet Template Library      |
+// |                                                                    |
+// | Copyright (c) 2002-2006                                            |
+// | Thorsten Raasch, Manuel Werner                                     |
+// +--------------------------------------------------------------------+
+
+#ifndef _WAVELETTL_JL_INDEX_H
+#define _WAVELETTL_JL_INDEX_H
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+namespace WaveletTL
+{
+  /*!
+    Index class for the multiwavelet basis in [0,1] from [JL],
+    modelling a multiindex of the form lambda=(j,e,c,k).
+
+    We internally assume that j0==1.
+  */
+  class JLIndex
+  {
+  public:
+    //! default constructor
+    JLIndex();
+
+    //! type index type
+    typedef int type_type;
+
+    //! component type
+    typedef int component_type;
+
+    //! translation index type
+    typedef int translation_type;
+
+    //! constructor with specified parameters
+    JLIndex(const int j, const type_type e, const component_type c, const translation_type k);
+
+    //! assignment
+    JLIndex& operator = (const JLIndex& lambda);
+
+    //! check equality
+    bool operator == (const JLIndex& lambda) const;
+
+    //! check non-equality
+    bool operator != (const JLIndex& lambda) const
+    { return !(*this == lambda); }
+    
+    //! preincrement
+    JLIndex& operator ++ ();
+    
+    //! lexicographic order <
+    bool operator < (const JLIndex& lambda) const;
+
+    //! lexicographic order <=
+    bool operator <= (const JLIndex& lambda) const
+    { return (*this < lambda || *this == lambda); }
+    
+    //! scale j
+    const int j() const { return j_; }
+    
+    //! type e
+    const type_type& e() const { return e_; }
+    
+    //! component type c
+    const component_type& c() const { return c_; }
+
+    //! translation index k
+    const translation_type& k() const { return k_; }
+
+  protected:  
+    
+    //! scale, type, component, translation
+    int j_, e_, c_, k_;
+    
+  };
+
+  //! stream output
+  std::ostream& operator << (std::ostream& os, const JLIndex& lambda)
+  {
+    using namespace std;
+    os << "("
+       << lambda.j()
+       << ","
+       << lambda.e()
+       << ","
+       << lambda.c()
+       << ","
+       << lambda.k()
+       << ")";
+    return os;
+  }
+
+  /*!
+    index of first (leftmost) generator on level j >= j0
+  */
+  JLIndex first_generator(const int j);
+
+  /*!
+    index of last (rightmost) generator on level j >= j0
+  */
+  JLIndex last_generator(const int j);
+
+  /*!
+    index of first (leftmost) wavelet on level j >= j0
+  */
+  JLIndex first_wavelet(const int j);
+
+  /*!
+    index of last (rightmost) wavelet on level j >= j0
+  */
+  JLIndex last_wavelet(const int j);
+}
+
+// include implementation
+#include <interval/jl_index.cpp>
+
+#endif

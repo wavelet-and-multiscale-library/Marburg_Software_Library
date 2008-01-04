1476de90ec205fb8ab5d8017ad4ceea3d2e5f656
raasch <raasch>
2008-01-04 10:45:15 +0000

    re-enabled a branch in the test program

--- tests/test_full_gramian.cpp
+++ tests/test_full_gramian.cpp
@@ -91,9 +91,9 @@ int main()
   cout << "* Gramian matrix on next level j0+1=" << basis.j0()+1 << ":" << endl
        << G;
 
-#if 0
+#if 1
 
-  const unsigned int testcase=1;
+  const unsigned int testcase=2;
   Function<1>* u = 0;
 
   switch(testcase) {

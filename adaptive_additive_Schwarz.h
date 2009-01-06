44263f08c57520eb84da0ec19aa9160e21d402be
werner <werner>
2009-01-06 09:25:39 +0000

    add forgotten file adaptive_additive_Schwarz.h to repository

---
+++ adaptive_additive_Schwarz.h
@@ -0,0 +1,39 @@
+// -*- c++ -*-
+
+// +--------------------------------------------------------------------+
+// | This file is part of FrameTL - the Wavelet Template Library      |
+// |                                                                    |
+// | Copyright (c) 2002-2005                                            |
+// | Thorsten Raasch, Manuel Werner                                     |
+// +--------------------------------------------------------------------+
+
+#ifndef _FRAME_TL_ADDITIVE_H
+#define _FRAME_TL_ADDITIVE_H
+
+#include <algebra/infinite_vector.h>
+
+namespace FrameTL
+{
+
+
+  /*!
+    adaptive additive Schwarz method from [DRSW]
+   */
+  template <class PROBLEM>
+  void AddSchw(const PROBLEM& P, const double epsilon,
+		 Array1D<InfiniteVector<double, typename PROBLEM::Index> >& approximations);
+
+
+//   /*!
+//     adaptive multiplicative Schwarz method from [DRSW]
+//     tuned version
+//    */
+//   template <class PROBLEM>
+//   void  adaptive_multiplicative_Schwarz_SOLVE(const PROBLEM& P,  const double epsilon,
+// 					      Array1D<InfiniteVector<double, typename PROBLEM::Index> >& approximations);
+
+}
+
+#include <adaptive_additive_Schwarz.cpp>
+
+#endif

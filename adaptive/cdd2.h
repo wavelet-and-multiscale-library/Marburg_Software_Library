6ab445e01a371625f5411271caabf024066d8009
keding <keding>
2016-01-28 10:41:55 +0000

    added DKOR(Dahlke, Oswald, Keding, Raasch) strategy for quarklet frames made some minor changes

--- adaptive/cdd2.h
+++ adaptive/cdd2.h
@@ -41,12 +41,16 @@ namespace WaveletTL
   template <class PROBLEM>
   void CDD2_SOLVE(const PROBLEM& P, const double nu, const double epsilon,
 		         InfiniteVector<double, typename PROBLEM::WaveletBasis::Index>& u_epsilon,
-                         const unsigned int maxlevel = 12);
+                         InfiniteVector<double, double>& residuals,
+                         const unsigned int maxlevel = 12, const CompressionStrategy strategy = CDD1,
+                         const int pmax = 0, const double a = 0, const double b = 0);
+                         
   /* same with int instead of Index */
   template <class PROBLEM>
   void CDD2_SOLVE(const PROBLEM& P, const double nu, const double epsilon,
 		         InfiniteVector<double, int>& u_epsilon,
                          const unsigned int maxlevel = 12);
+
  
 }
 

bc986243ab1dea9171e3c62968471a7d333fd8a2
keding <keding>
2016-01-28 11:14:34 +0000

    added plots added jmax for the bases

--- tests/test_cdd2.cpp
+++ tests/test_cdd2.cpp
@@ -8,6 +8,7 @@
 #include <numerics/iteratsolv.h>
 
 #include <interval/i_index.h>
+#include <interval/i_indexplot.h>
 #include <interval/ds_basis.h>
 #include <galerkin/sturm_equation.h>
 #include <galerkin/cached_problem.h>
@@ -84,8 +85,11 @@ int main()
   const int dT = 3;
   typedef DSBasis<d,dT> Basis;
   typedef Basis::Index Index;
+  Basis basis(true, true);
+  int jmax = 8;
+  basis.set_jmax(jmax);
 
-  SturmEquation<Basis> problem(T);
+  SturmEquation<Basis> problem(T,basis);
   CachedProblem<SturmEquation<Basis> > cproblem(&problem);
 
   InfiniteVector<double, Index> F_eta;
@@ -93,7 +97,30 @@ int main()
   const double nu = cproblem.norm_Ainv() * l2_norm(F_eta);
   cout << "nu = " << nu << endl;
   InfiniteVector<double, Index> u_epsilon;
-  CDD2_SOLVE(cproblem, nu, 1e-1, u_epsilon);
+  CDD2_SOLVE(cproblem, nu, 1, u_epsilon, jmax);
+  
+  
+  /* plot solution coefficients */
+    std::ofstream coeff_stream1("../../Arbeitsfläche/plotcoeff.m");
+#if 1
+    coeff_stream1 << "figure;" << endl;
+    plot_indices(&basis, u_epsilon, jmax, coeff_stream1, "jet", false, true, -8);
+    coeff_stream1 << "title('Example [CDD2]: solution coefficients of test problem');" << endl;
+    coeff_stream1.close();
+
+    /* plot solution graph */
+    u_epsilon.scale(&cproblem, -1); /* scaling because ... */
+    SampledMapping<1> sm1(evaluate(cproblem.basis(), u_epsilon, true, 2*jmax)); // Increase last parameter, if "Assertion `resolution >= 0' failed."
+    std::ofstream u_stream1("../../Arbeitsfläche/plotgraph.m");
+    sm1.matlab_output(u_stream1);
+    u_stream1 << "figure;\nplot(x,y);"
+              << "title('Example [CDD2]: solution to test problem');" << endl;
+    u_stream1.close();
+#endif
   
   return 0;
 }
+
+
+
+

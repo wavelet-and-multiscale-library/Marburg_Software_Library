ccd735059cb498c0865bf3d29e85bfa1c5ce67f1
keding <keding>
2016-01-28 11:19:29 +0000

    added a plot

--- tests/test_periodic_gramian.cpp
+++ tests/test_periodic_gramian.cpp
@@ -41,6 +41,30 @@ public:
   }
 };
 
+template <unsigned int N>
+class PeriodicTestProblem
+  : public PeriodicLaplacianProblem
+{
+public:
+  double g(const double t) const {
+    switch(N) {
+    case 1:
+      //u(t) = 100*(t*t*t-1.5*t*t+0.5*t)
+        //return 100*(t*t*t-1.5*t*t+0.5*t);
+        return t*(1-t);
+      //return 100*(-6*t+3);
+      break;
+    case 2:
+      return sin(2*M_PI*t);
+    case 3:
+      return cos(2*M_PI*t);      
+    default:
+      return 0;
+      break;
+    }
+  }
+};
+
 int main()
 {
   cout << "Testing periodic Gramians ..." << endl;
@@ -51,9 +75,10 @@ int main()
   typedef PeriodicBasis<RBasis> Basis;
   typedef Basis::Index Index;
   Basis basis;
+  PeriodicTestProblem<1> tper;
 
   typedef PeriodicIntervalGramian<RBasis> Problem;
-  Problem G(basis, InfiniteVector<double,Index>());
+  Problem G(tper, basis);
   
   const unsigned int testcase=2;
   Function<1>* u = 0;
@@ -72,21 +97,27 @@ int main()
   cout << "* compute approximate expansions of the test function for several levels..." << endl;
   const int jmin = basis.j0();
 //   const int jmax = jmin;
-  const int jmax = 10;
+  const int jmax = 4;
   Vector<double> js(jmax-jmin+1);
   Vector<double> Linfty_errors(jmax-jmin+1), L2_errors(jmax-jmin+1);
 
   Vector<double> rhs;
+  InfiniteVector<double, Index> rhsinf;
+  
 
   for (int j = jmin; j <= jmax; j++) {
     cout << "  j=" << j << ":" << endl;
     js[j-jmin] = j;
 
     // compute expansion coefficients of u in the dual basis
-    basis.expand(u, false, j, rhs);
-//     cout << "  inner products of u against all wavelets on level " << j << ":" << endl
+    basis.expand(u, true, j, rhs);
+//    cout << "  inner products of u against all wavelets on level " << j << ":" << endl
 //   	 << rhs << endl;
-
+    
+    basis.expand(u, false, j, rhsinf);
+//    cout << "  inner products of u against all wavelets on level " << j << " (InfiniteVector-version):" << endl
+//   	 << rhsinf << endl;
+    
     // setup the set Lambda of active wavelets
     set<Index> Lambda;
     for (Index lambda = basis.first_generator(basis.j0());; ++lambda) {
@@ -105,8 +136,11 @@ int main()
     
     // solve Gramian system
     Vector<double> uj(A.row_dimension()), residual(A.row_dimension(), false);
+    InfiniteVector<double, Index> ujinf, residualinf;
+    
     unsigned int iterations;
     CG(A, rhs, uj, 1e-15, 250, iterations);
+    //CG(A, rhsinf, ujinf, 1e-15, 250, iterations);
 //     cout << "  solution coefficients: " << uj << endl;
     cout << "  Galerkin system solved with residual (infinity) norm ";
     A.apply(uj, residual);
@@ -144,9 +178,24 @@ int main()
     const double L2_error = sqrt(l2_norm_sqr(uj_values-uexact_values)*h);
     cout << "  L_2 error on a subgrid: " << L2_error << endl;
     L2_errors[j-jmin] = L2_error;
+    
+//    cout << uj << endl;
+//    cout << rhsinf << endl;
   }
 
   if (u) delete u;
+  
+  
+  
+  
+  //solution1_epsilon.scale(&cproblem, -1);
+  //cout << solution1_epsilon << endl;
+  SampledMapping<1> sm1(basis.evaluate(rhsinf, 8));
+  std::ofstream u_stream1("../../Desktop/plotthis.m");
+  sm1.matlab_output(u_stream1);
+  u_stream1 << "figure;\nplot(x,y);"
+            << "title('mygraph');" << endl;
+  u_stream1.close();
 
   return 0;
 }
